785fffa1e4004af76e86b4cece8e14c55e0fe8d8
Nathan <natedog2424mail@gmail.com>
2021-03-16 20:29:50 -0700

    templates and ajax

--- amunWebServer/amunWebServer.py
+++ amunWebServer/amunWebServer.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template("home.html")
+
+@app.route('/test', methods = ['POST'])
+def test():
+    print("test")
+    return render_template("home.html")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
