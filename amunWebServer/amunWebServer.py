432f61d6cc5abd058c2cdea6a73610166b48e259
Nathan <natedog2424mail@gmail.com>
2021-03-18 00:41:38 -0700

    color picker setup

--- amunWebServer/amunWebServer.py
+++ amunWebServer/amunWebServer.py
@@ -2,17 +2,25 @@ from flask import Flask, render_template, request
 import serial
 
 app = Flask(__name__)
-light = False;
 @app.route('/')
 def hello_world():
     return render_template("home.html")
 
 @app.route('/test', methods = ['POST'])
 def test():
-    print(request.form.items())
+    color = request.form['color']
+    ##color = color[1:]
+    print(color)
+    try:
+        ser.write(color.encode('utf-8'))
+    except:
+        print("no serial connection")
     return render_template("home.html")
 
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-    ser.flush()
+    try:
+        ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+        ser.flush()
+    except:
+        print("no arduino connected")
     app.run(debug=True, host='0.0.0.0', port=80)
